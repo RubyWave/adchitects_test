3b0c5ce6aeb59344efe728ec6b0752309a47e068
Karl <karol@fooz.pl>
2023-08-06 17:45:26 +0200

    added learning palace bolierplate theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * adds autoloader from composer
+ */
+if (file_exists( dirname(__FILE__) . '/vendor/autoload.php')) {
+    $loader = require_once 'vendor/autoload.php';
+} else {
+    return;
+}
+
+
+new LearningPalace\EnqueStylesScripts();
+
+
