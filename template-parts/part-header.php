bc74fb3e5f825f5fe343b3aacafd76070424a034
Karl <karol@fooz.pl>
2023-08-08 16:55:16 +0200

    added failsafe for case on not setting up notification text

--- template-parts/part-header.php
+++ template-parts/part-header.php
@@ -2,15 +2,19 @@
 use LearningPalace\Menus;
 
 $themeUrl = wp_normalize_path( get_stylesheet_directory_uri() );
+$notificationText = get_option('topnotification');
 ?>
 
-<header class="page-header">
-    <div class="extra-notification">
-        <div class="container">
-            <p class="extra-notification-text"><?php echo get_option('topnotification'); ?></p>
-            <img class="extra-notification-close" src="<?php echo $themeUrl; ?>/img/close.svg">
+<header class="page-header <?php if($notificationText == '') echo 'extra-info-hidden' ?>">
+    <?php if($notificationText == ''): ?>
+        <div class="extra-notification">
+            <div class="container">
+                <p class="extra-notification-text"><?php echo get_option('topnotification'); ?></p>
+                <img class="extra-notification-close" src="<?php echo $themeUrl; ?>/img/close.svg">
+            </div>
         </div>
-    </div>
+    <?php endif; ?>
+    
     <div class="container main-header-container">
         <a href="<?php echo get_home_url(); ?>" class="page-logo">
             <img class="" src="<?php echo $themeUrl; ?>/img/logo@2x.svg">
